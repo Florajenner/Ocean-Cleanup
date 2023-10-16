52a7aaa2803c76b21a35fb297d204f7bb7be646c
FLORA JENNER <flora.jenner@gmail.com>
2023-10-16 15:23:00 +0000

    create a function that creates a 2d board showing P for plastic

--- run.py
+++ run.py
@@ -3,3 +3,14 @@
 ROWS, COLS = 10, 10
 AMOUNT_OF_RUBBISH = 0.4
 grid = []
+
+​
+def create_ocean():
+    """
+    This function creates the 2D game board
+    """
+    # Number of 'P' cells board will have plastic rubbish in
+    num_P = int(AMOUNT_OF_RUBBISH * ROWS * COLS)
+​
+    # Create a 1D list with 'P' and empty spaces
+    grid_list = ['P'] * num_P + ['-'] * (ROWS*COLS - num_P)
